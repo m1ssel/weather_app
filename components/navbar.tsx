aa067074bc30edcff962de6495a5e77be8501204
m1ssel <artemaz665@gmail.com>
2025-02-06 19:04:18 +0100

    created searchBox component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,37 +1,17 @@
-import React from 'react';
-import { FaSearch } from 'react-icons/fa';
-import Image from 'next/image';
+import React from "react";
+// import { FaSearch } from 'react-icons/fa';
+import SearchBox from "./searchBox";
+import Image from "next/image";
 
-type Props = {
-    value: string;
-    onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined;
-    onSubmit?: React.FormEventHandler<HTMLFormElement> | undefined;
-};
-
-const Navbar = (props: Props) => {
-    return (
-        <nav className='shadow-sm sticky top-0 left-0 z-50 text-white'>
-            <div className='h-20 flex items-center justify-around bg-nav'>
-                <div>
-                    <Image
-                        src='/chill.webp'
-                        alt='logo'
-                        width={150}
-                        height={150}
-                    />
-                    {/* <img src='chill.jpg' alt='asd' /> */}
-                </div>
-                {/* <div className='flex items-center bg-primary'>
-                    <input
-                        type='text'
-                        placeholder='Search for your city'
-                        className='placeholder:text-gray-400 text-black outline-none pl-2 py-2 rounded-l-lg'
-                    />
-                    <div className='flex items-center justify-center text-gray-400 p-2 h-10 w-11 border-l-[2px] border-r-0 border-t-0 border-b-0 border-solid cursor-pointer bg-white rounded-r-lg'>
-                        <FaSearch />
-                    </div>
-                </div> */}
-                <form
+const Navbar = () => {
+  return (
+    <nav className="shadow-sm sticky top-0 left-0 z-50 text-white">
+      <div className="h-20 flex items-center justify-around bg-nav">
+        <div>
+          <Image src="/chill.webp" alt="logo" width={150} height={150} />
+          {/* <img src='chill.jpg' alt='asd' /> */}
+        </div>
+        {/* <form
                     onSubmit={props.onSubmit}
                     className='flex items-center bg-primary'
                 >
@@ -45,10 +25,11 @@ const Navbar = (props: Props) => {
                     <button className='flex items-center justify-center text-gray-400 p-2 h-10 w-11 border-l-[2px] bg-blue-500 hover:bg-blue-600 rounded-r-lg'>
                         <FaSearch className='text-white' />
                     </button>
-                </form>
-            </div>
-        </nav>
-    );
+                </form> */}
+        <SearchBox />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- components/slider.tsx
+++ components/slider.tsx
@@ -1,23 +1,23 @@
-import { MdArrowForwardIos } from 'react-icons/md';
-import { IoRainy } from 'react-icons/io5';
+import { MdArrowForwardIos } from "react-icons/md";
+import { IoRainy } from "react-icons/io5";
 
 const Slider = () => {
-    return (
-        <div className='flex items-center justify-between'>
-            <div>
-                <MdArrowForwardIos className='-scale-x-100' />
-            </div>
-            <div className='flex flex-col items-center'>
-                <div className='uppercase'>Sun</div>
-                <div>
-                    <IoRainy className='w-[2rem] h-auto' />
-                </div>
-            </div>
-            <div>
-                <MdArrowForwardIos />
-            </div>
+  return (
+    <div className="flex items-center justify-between">
+      <div>
+        <MdArrowForwardIos className="-scale-x-100" />
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="uppercase">Sun</div>
+        <div>
+          <IoRainy className="w-[2rem] h-auto" />
         </div>
-    );
+      </div>
+      <div>
+        <MdArrowForwardIos />
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
