4747d9a9132b55d496bb01dc261a774f4bb2fe81
m1ssel <artemaz665@gmail.com>
2025-03-09 23:34:48 +0100

    changes

--- components/searchBox.tsx
+++ components/searchBox.tsx
@@ -10,24 +10,26 @@ type Props = {
 
 const SearchBox = (props: Props) => {
   return (
-    <form
-      onSubmit={props.onSubmit}
-      className={cn(
-        "flex relative items-center justify-center h-10",
-        props.className
-      )}
-    >
-      <input
-        type="text"
-        value={props.value}
-        onChange={props.onChange}
-        placeholder="Search location"
-        className="px-4 py-2 w-[230px] border border-solid border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 h-full"
-      />
-      <button className="px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 h-full">
-        <IoSearch />
-      </button>
-    </form>
+    <>
+      <form
+        onSubmit={props.onSubmit}
+        className={cn(
+          "flex relative items-center justify-center h-10",
+          props.className
+        )}
+      >
+        <input
+          type="text"
+          value={props.value}
+          onChange={props.onChange}
+          placeholder="Search location"
+          className="px-4 py-2 w-[230px] border border-solid border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 h-full"
+        />
+        <button className="px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 h-full">
+          <IoSearch />
+        </button>
+      </form>
+    </>
   );
 };
 
